c3ae277d45c74b61d79011e639e96e0ac022d581
Mike <you@example.com>
2024-09-04 17:24:33 +1100

    Изменил стили chadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <Button variant='outline'>Корзина</Button>
+    </>
+ 
   );
 }
