86f8df1d4d7804b169bfbcdc165296ab079e0d60
Mike <saburov1985@gmail.com>
2024-09-05 17:38:04 +1100

    создал папку для постоянных компонент типа header etc

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 
 const nunito = Nunito({
-  subsets: ['cyrillic'],
-  variable: '--font-nunito',
-  weight: ['400', '500', '600', '700', '800', '900'],
+  subsets: ["cyrillic"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
