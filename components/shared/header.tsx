4faa5f571b602c7fcdc894b74f9e643079469085
Mike <saburov1985@gmail.com>
2024-09-06 12:41:11 +1100

    добавлен компонент container для разметки и сверстан блок с логотипом в header. Header подключен к layout для отображения на всех страницах

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Container } from "./container";
+import Image from "next/image";
 
-interface Props {
-className?: string;
+interface headerProps {
+  className?: string;
 }
 
-export const header: React.FC<Props> = ({'className'}) => {
- return (
-<div className={}></div>
-);
-}
+export const Header: React.FC<headerProps> = ({ className }) => {
+  return (
+    <header className={cn("border border-b", className)}>
+      <Container className="flex items-center justify-between py-8">
+        {/* Левая часть */}
+        <div className="flex items-center gap-4">
+          <Image src={"/logo.png"} alt="Logo" width={35} height={35} />
+          <div>
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">
+              вкусней уже некуда
+            </p>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
