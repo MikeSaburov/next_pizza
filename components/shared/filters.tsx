f4770f842b86fee0cce833d7f00f12b2c235812d
Mike <saburov1985@gmail.com>
2024-09-09 15:29:44 +1100

    feature: импорт FilterCheckbox на главную

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Title } from "./title";
+import { FilterCheckbox } from "./filter-checkbox";
 
 interface filtersProps {
   className?: string;
@@ -9,6 +10,11 @@ export const Filters: React.FC<filtersProps> = ({ className }) => {
   return (
     <div className={className}>
       <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
+
+      <div className="flex flex-col gap-4">
+        <FilterCheckbox text="Можно собрать" value="1" />
+        <FilterCheckbox text="Новинки" value="2" />
+      </div>
     </div>
   );
 };
