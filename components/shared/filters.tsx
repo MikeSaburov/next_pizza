ec95156f4627d59977976a4163249950c7b4c8e2
Mike <saburov1985@gmail.com>
2024-09-09 17:25:56 +1100

    feature: создать и набросать верстку для раздела фильрация по ингридиентам

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -3,6 +3,7 @@ import { Title } from "./title";
 import { FilterCheckbox } from "./filter-checkbox";
 import { Input } from "../ui";
 import { RangeSlider } from "./range-slider";
+import { CheckboxFiltersGroup } from "./checkbox-filters-group";
 
 interface filtersProps {
   className?: string;
@@ -13,11 +14,12 @@ export const Filters: React.FC<filtersProps> = ({ className }) => {
     <div className={className}>
       <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
 
+      {/* Верхние чекбоксы */}
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="Можно собирать" value="1" />
         <FilterCheckbox text="Новинки" value="2" />
       </div>
-
+      {/* Фильтрация по цене */}
       <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
         <p className="font-bold mb-3">Цена от и до:</p>
         <div className="flex gap-3 mb-5">
@@ -38,6 +40,8 @@ export const Filters: React.FC<filtersProps> = ({ className }) => {
         </div>
         <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
       </div>
+      {/* Фильтрация по ингрединтам */}
+      <CheckboxFiltersGroup title={""} items={[]} />
     </div>
   );
 };
