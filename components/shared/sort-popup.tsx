d4d4551537dc2757e51132176c0990609e3eff89
Mike <saburov1985@gmail.com>
2024-09-09 12:19:26 +1100

    feture: создал шаблон компонента sort-popup

---
+++ components/shared/sort-popup.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface SortPopupProps {
+  className?: string;
+}
+
+export const SortPopup: React.FC<SortPopupProps> = ({ className }) => {
+  return <div className={className}></div>;
+};
