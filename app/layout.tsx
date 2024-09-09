88977ccef5eaec5924769c3424b637632348d175
Mike <saburov1985@gmail.com>
2024-09-09 12:36:09 +1100

    refactor: 'измеить иконку на вкладке браузера'

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" sizes="any" />
+      </head>
       <body className={nunito.className}>
         <main className="min-h-screen">
           <Header />
