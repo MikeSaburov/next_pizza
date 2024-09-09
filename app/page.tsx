90ab1eaaf5910ec6d057f276ef23d60bb92cfb86
Mike <saburov1985@gmail.com>
2024-09-09 15:00:26 +1100

    feature: разбил страницу на блок для фильтров и список товаров

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,23 @@ import { Container, Title, TopBar } from "@/components/shared";
 export default function Home() {
   return (
     <>
+      {/* Заголовок страницы */}
       <Container className="mt-5">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
+      {/* Липкий топбар */}
       <TopBar />
+
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+          {/* Блок фильтрации */}
+          <div className="w-[250px]">Фильтры</div>
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Список товаров</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
