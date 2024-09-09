4668058b0e22545f9db7f254657df2fbee7fce99
Mike <saburov1985@gmail.com>
2024-09-09 11:53:27 +1100

    добавить заголовок

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Container, Title } from "@/components/shared";
 export default function Home() {
   return (
     <>
-      <Container>
+      <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
     </>
