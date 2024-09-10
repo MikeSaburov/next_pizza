519b1dc762e6caea98e9c010f06eed517ac07bbc
Mike <saburov1985@gmail.com>
2024-09-10 14:01:16 +1100

    refactor: выравнил экспорты

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,13 +1,13 @@
-export {ProductCard} from './product-card';
-export {FilterCheckbox} from './filter-checkbox';
-export {Filters}  from './filters';
+export { ProductCard } from './product-card';
+export { FilterCheckbox } from './filter-checkbox';
+export { Filters }  from './filters';
 export { TopBar } from './top-bar';
 export { SortPopup } from './sort-popup';
 export { Categories } from './categories';
-export {Container} from './container'
-export {Header} from './header'
+export { Container } from './container'
+export { Header } from './header'
 export { Title } from './title'
-export {ProductsGroupList} from './products-group-list';
+export { ProductsGroupList } from './products-group-list';
 
 
 
