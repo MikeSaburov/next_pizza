54ab4ab8edbff51b51d62121ae69b0a0e73a5aec
Mike <saburov1985@gmail.com>
2024-09-10 16:13:44 +1100

    refactor: массив категорий переделан в обуъект и релизован переход по вкладкам при скролле

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -9,14 +9,13 @@ interface categoriesProps {
 }
 
 const cats = [
-  "Пиццы",
-  "Комбо",
-  "Закуски",
-  "Коктейли",
-  "Кофе",
-  "Напитки",
-  "Десерты",
-  "Десерты",
+  { id: 1, name: "Пиццы" },
+  { id: 2, name: "Комбо" },
+  { id: 3, name: "Закуски" },
+  { id: 4, name: "Коктейли" },
+  { id: 5, name: "Кофе" },
+  { id: 6, name: "Напитки" },
+  { id: 7, name: "Десерты" },
 ];
 
 export const Categories: React.FC<categoriesProps> = ({ className }) => {
@@ -26,11 +25,11 @@ export const Categories: React.FC<categoriesProps> = ({ className }) => {
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
     >
-      {cats.map((name, index) => (
+      {cats.map(({ name, id }, index) => (
         <a
           className={cn(
             "flex items-center font-bold h-11 rounded-2xl px-5",
-            categoryActiveId === index + 1 &&
+            categoryActiveId === id &&
               "bg-white shadow-md shadow-gray-200 text-primary"
           )}
           key={index}
