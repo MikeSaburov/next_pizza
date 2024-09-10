dd32027ee29be537b0e92f56d0f77cad1bb80ecc
Mike <saburov1985@gmail.com>
2024-09-10 15:54:55 +1100

    feature: реализовать переключения вкладок групп товаров при скролле

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+
+import React, { useActionState } from "react";
 import { cn } from "@/lib/utils";
+import { useCategoryStore } from "@/store/category";
 
 interface categoriesProps {
   className?: string;
@@ -15,9 +18,10 @@ const cats = [
   "Десерты",
   "Десерты",
 ];
-const activeIndex = 0;
 
 export const Categories: React.FC<categoriesProps> = ({ className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
+
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
@@ -26,7 +30,7 @@ export const Categories: React.FC<categoriesProps> = ({ className }) => {
         <a
           className={cn(
             "flex items-center font-bold h-11 rounded-2xl px-5",
-            activeIndex === index &&
+            categoryActiveId === index + 1 &&
               "bg-white shadow-md shadow-gray-200 text-primary"
           )}
           key={index}
