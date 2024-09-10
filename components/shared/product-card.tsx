7cfca2b7070287024068e56cbb9ec7ed2cbe5a38
Mike <saburov1985@gmail.com>
2024-09-10 12:48:59 +1100

    refactor: изменил макс и мин размеры картинок пицц

--- components/shared/product-card.tsx
+++ components/shared/product-card.tsx
@@ -23,7 +23,11 @@ export const ProductCard: React.FC<productCardProps> = ({
     <div className={className}>
       <Link href={`/product/${id}`}>
         <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260 px]">
-          <img className="w-[215px] h-[215px]" src={imageUrl} alt={name} />
+          <img
+            className="max-w-[215px] min-w-[100px] min-h-[100px] max-h-[215px]"
+            src={imageUrl}
+            alt={name}
+          />
         </div>
         <Title text={name} size={"sm"} className="mb-1 mt-3 font-bold" />
         <p className="text-sm text-gray-400">
