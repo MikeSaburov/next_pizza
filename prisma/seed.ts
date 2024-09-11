7d54b72ad16fee04f3bec68184470f0347bd207b
Mike <saburov1985@gmail.com>
2024-09-11 18:00:51 +1100

    feature: набросал функции в seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,38 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { create } from 'zustand';
+import { prisma } from "./prisma-client";
 
+
+// генерируем даннные
 async function up() {
-    
+    await prisma.user.createMany({
+        data: [
+            {
+                fullName: "admin",
+                email: "admin@test.ru",
+                password: hashSync("admin",10),
+                verified: new Date(),
+                role: "ADMIN",
+            },
+            {
+                fullName: "user",
+                email: "usern@test.ru",
+                password: "user",
+                role: "USER",
+            },
+        ]
+    })
 }
+
+// очищаем даннные
 async function down() {
 
 }
 async function main() {
+    try {
+        await down();
+        await up();
+    } catch (e) {
+        console.log(e);
+    }
 
 }
